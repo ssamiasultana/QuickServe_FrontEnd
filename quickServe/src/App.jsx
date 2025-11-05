55e7457bf42a38654d69ea56f6edefe10606c9c8
ssamiasultana <samiabracu@gmail.com>
2025-11-06 00:09:29 +0600

    feat: add react-router for routing and update App component

--- quickServe/src/App.jsx
+++ quickServe/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link } from 'react-router'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
    <div className='text-red-500 items-center justify-center flex h-screen'>
     hello world
+   
    </div>
   )
 }
